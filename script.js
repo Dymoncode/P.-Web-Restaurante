9355f8434e86e818d078360824ebb0ab8e477ec9
Andres <andresdmb96@gmail.com>
2025-12-20 10:37:57 -0400

    Agregado formulario al presionar el boton 'contacto' en el menu nav

--- script.js
+++ script.js
@@ -2,6 +2,18 @@ const toggleButton = document.getElementById("themeToggle");
 const body = document.body;
 
 toggleButton.addEventListener("click", () => {
-    body.classList.toggle("dark");
-    toggleButton.textContent = body.classList.contains("dark") ? "☀️" : "🌙";
+  body.classList.toggle("dark");
+  toggleButton.textContent = body.classList.contains("dark") ? "☀️" : "🌙";
 });
+
+// BOTON PARA MOSTRAR FORMULARIO DE CONTACTO
+const btnContacto = document.getElementById("contacto-menu");
+
+btnContacto.addEventListener("click", mostrarFormulario);
+
+function mostrarFormulario() {
+  const formulario = document.querySelector("form");
+  if (formulario.classList.contains("hidden")) {
+    formulario.classList.remove("hidden");
+  }
+}
